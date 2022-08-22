215d6d980833366394d8c36815c8079a7a2aa29c
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2022-08-22 20:46:24 +0700

    Time: 257 ms (52.25%), Space: 15.4 MB (86.45%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,13 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        numsMap = {}
-        for i in nums:
-            if i in numsMap:
-                numsMap[i] += 1
-            else:
-                numsMap[i] = 1
-        maxEntry = [0, 0]
-        for k, v in numsMap.items():
-            if v > maxEntry[1]:
-                maxEntry = [k, v]
-        return maxEntry[0]
+        # numsMap = {}
+        # for i in nums:
+        #     if i in numsMap:
+        #         numsMap[i] += 1
+        #     else:
+        #         numsMap[i] = 1
+        # maxEntry = [0, 0]
+        # for k, v in numsMap.items():
+        #     if v > maxEntry[1]:
+        #         maxEntry = [k, v]
+        # return maxEntry[0]
+        nums.sort()
+        return nums[(len(nums) - 1) // 2]
