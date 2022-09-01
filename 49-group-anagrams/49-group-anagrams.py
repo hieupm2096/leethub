0e47ef990ace531bf95ebc4ba9375000b49655bc
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2022-09-01 15:30:47 +0700

    Time: 244 ms (9.22%), Space: 19.8 MB (25.94%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,8 +1,14 @@
 class Solution:
+    def getKey(self, word: str) -> tuple:
+        chars = [0] * 26
+        for char in word:
+            chars[ord(char) - ord('a')] += 1
+        return tuple(chars)
+    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         maps = {}
         for i in strs:
-            tmp = ''.join(sorted(i))
+            tmp = self.getKey(i)
             if tmp in maps:
                 maps[tmp].append(i)
             else:
