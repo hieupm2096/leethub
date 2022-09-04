388973406096564e37330d75729ccba5178f0ae4
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2022-09-04 17:13:59 +0700

    Time: 209 ms (20.16%), Space: 18.5 MB (91.96%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,11 +2,23 @@ from collections import defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # d is a hashmap(referred as dictionary in python)
+        if len(nums) < 2:
+            return nums
         d = defaultdict(lambda: 0)
         for i in nums:
             d[i] += 1
-        keys = list(d.keys())
-        keys.sort(key=lambda x: (-d[x], x))
-        return keys[0:k]
+        bucket = [None] * (len(nums) + 1)
+        for key, value in d.items():
+            if not bucket[value]:
+                bucket[value] = []
+            bucket[value].append(key)
+        rs = []
+        for i in range(len(bucket) - 1, -1, -1):
+            if not bucket[i]:
+                continue
+            if len(rs) < k:
+                rs += bucket[i]
+            else:
+                break
+        return rs[:k]
             
